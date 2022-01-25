6b9319c524c4f7bac791244476ff9f3147b224a2
isabelanery <isabelanery242@gmail.com>
2022-01-25 19:39:59 -0300

    Adiciona requisito 6.

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,31 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
-}
+const { species } = data;
+
+const countAllAnimals = () => species
+.reduce((acc, animal) => {
+    const { name, residents } = animal; 
+    return { ...acc, [name]: residents.length };
+}, {});
+
+const countAnimals = (animal) => {
+  if (!animal) { 
+    return countAllAnimals();
+  }
+
+  if (animal.sex === undefined) {
+    return species.find((animalSpecie) => animalSpecie.name === animal.specie).residents.length;
+  }
+
+  if (animal.specie && animal.sex !== undefined) {
+    return species
+    .find((animalSpecie) => animalSpecie.name === animal.specie)
+    .residents
+    .filter((resident) => resident.sex === animal.sex)
+    .length;
+  }
+};
 
 module.exports = countAnimals;
+
+// console.log(species);
