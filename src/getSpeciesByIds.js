8e8ae3442304f3340b1c9f9db794d4f7e7a0d7e1
isabelanery <isabelanery242@gmail.com>
2022-01-25 15:10:01 -0300

    Adiciona requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,24 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+const species = data.species; 
+
+const getSpeciesByIds = (...ids) => {
+  if (ids.length === 0) {
+    return [];
+  } 
+
+  return ids.map((searchId) => {
+    species.forEach((animal) =>  animal.id === searchId ? searchId = animal : searchId );
+
+    return searchId;
+  })
+
+  // const species = data.species; 
+  
 }
+
+// console.log(getSpeciesByIds('o'));
+
+// console.log(species);
 
 module.exports = getSpeciesByIds;
