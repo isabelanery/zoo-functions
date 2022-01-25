12d7001e4efa95d2a9c2f776daf408bebd118b9f
isabelanery <isabelanery242@gmail.com>
2022-01-25 15:59:54 -0300

    Lint no requisito 1 e portanto reestruturação do código. :clown:

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,24 +1,33 @@
 const data = require('../data/zoo_data');
 
-const species = data.species; 
-
 const getSpeciesByIds = (...ids) => {
-  if (ids.length === 0) {
-    return [];
-  } 
+  const listSpecies = ids.reduce((acc, curr) => {
+    data.species.forEach((animal) => {
+      if (curr === animal.id) {
+        acc.push(animal);
+      }
+    });
 
-  return ids.map((searchId) => {
-    species.forEach((animal) =>  animal.id === searchId ? searchId = animal : searchId );
+    return acc;
+  }, []);
 
-    return searchId;
-  })
-
-  // const species = data.species; 
-  
-}
-
-// console.log(getSpeciesByIds('o'));
-
-// console.log(species);
+  return listSpecies;
+};
 
 module.exports = getSpeciesByIds;
+
+// primeira tentativa que o lint não deixou ver a luz do sol 
+// conversando com o Paulo Victor, vi que a solução com o reduce fazia bem mais sentido 
+// e troquei a forma de resolver 
+
+// const listSpecies = ids.map((searchId) => {
+//   data.species.forEach((animal) => {
+//   // searchId === animal.id ? searchId = animal : searchId; 
+//     if (searchId === animal.id) {
+//       searchId = animal;
+//     } return searchId;
+// });
+//   return searchId;
+// });
+
+// return listSpecies; 
