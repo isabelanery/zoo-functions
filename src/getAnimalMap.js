79bba8b206ea02ac9d0816d190d856b8e4723b06
isabelanery <isabelanery242@gmail.com>
2022-01-25 22:37:56 -0300

    Adiciona teste 1 requisito 6.

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,28 @@
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
+const { species } = data;
+
+const locationAnimals = () => {
+  const mapAnimals = {
+    NE: [],
+    NW: [],
+    SE: [],
+    SW: [],
+  }
+
+  mapAnimals.NE = species.filter((animal) => animal.location === 'NE').map((specie) => specie.name);
+  mapAnimals.NW = species.filter((animal) => animal.location === 'NW').map((specie) => specie.name);
+  mapAnimals.SE = species.filter((animal) => animal.location === 'SE').map((specie) => specie.name);
+  mapAnimals.SW = species.filter((animal) => animal.location === 'SW').map((specie) => specie.name);
+
+  return mapAnimals;
 }
+
+const getAnimalMap = (options) => {
+  if (!options) {
+    return locationAnimals();
+  }
+
+};
 
 module.exports = getAnimalMap;
