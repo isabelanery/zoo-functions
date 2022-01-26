e688f8cb2015c710592f4a01bbba3dbbe7c19015
isabelanery <isabelanery242@gmail.com>
2022-01-26 00:12:12 -0300

    ggwp.

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -10,18 +10,67 @@ const locationAnimals = () => {
     SW: [],
   };
 
-  mapAnimals.NE = species.filter((animal) => animal.location === 'NE').map((specie) => specie.name);
-  mapAnimals.NW = species.filter((animal) => animal.location === 'NW').map((specie) => specie.name);
-  mapAnimals.SE = species.filter((animal) => animal.location === 'SE').map((specie) => specie.name);
-  mapAnimals.SW = species.filter((animal) => animal.location === 'SW').map((specie) => specie.name);
-
-  return mapAnimals;
+  return species
+    .reduce((acc, specie) => {
+      const { name, location } = specie;
+      (acc[location]).push(name);
+      return acc;
+    }, mapAnimals);
 };
+
+const namedAnimals = () => {
+  const mapAnimals = { NE: [], NW: [], SE: [], SW: [],}; 
+
+  return species
+    .reduce((acc, specie) => {
+      const { name, location, residents } = specie;
+      (acc[location].push(name)); 
+
+      const namingAnimal = {}; 
+      namingAnimal[name] = []; 
+      specie.name = namingAnimal;
+  
+      // residents.forEach((resident) => {
+
+      // });
+      return acc; 
+     }, mapAnimals);
+     
+
+}
+
+// const callMap = (animal) => { 
+//   const namingAnimal = {}; 
+//   namingAnimal[animal] = [];
+
+//   animal = namingAnimal;
+//   return animal;
+// }
+
+// const namedAnimals = () => {
+  
+//   const mapedAnimals = locationAnimals(); 
+
+//   mapedAnimals.NE = mapedAnimals.NE.map(callMap);
+
+//   return mapedAnimals;
+
+// }
 
 const getAnimalMap = (options) => {
   if (!options) {
     return locationAnimals();
   }
+
+  if (!options.includeNames || options.includeNames === false) {
+    return locationAnimals();
+  }
+
+  if (options.includeNames === true) {
+    return namedAnimals();
+  }
 };
 
 module.exports = getAnimalMap;
+
+console.log(getAnimalMap({includeNames: true }));
