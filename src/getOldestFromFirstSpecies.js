457ba7fed93f8fccdc6b3bb58fad12c76d45a130
isabelanery <isabelanery242@gmail.com>
2022-01-26 19:28:54 -0300

    Adiciona requisito 9.

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 const getOldestFromFirstSpecies = (id) => {
-  // escreva
+  const animalId = data.employees.find((employee) => employee.id === id).responsibleFor[0];
+  // console.log(animalId);
+  return Object.values(data.species.find((animal) => animal.id === animalId)
+    .residents.sort((a, b) => b.age - a.age)[0]);
 };
 
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 module.exports = getOldestFromFirstSpecies;
