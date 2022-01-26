abaf604cbd946775d111431e8b8ea77bd014a036
isabelanery <isabelanery242@gmail.com>
2022-01-26 17:28:54 -0300

    Adiciona requisito 7 - jeitinho brasileiro

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,58 +1,129 @@
 const data = require('../data/zoo_data');
 
+// Todo o código realizado em parceria com Paulo Victor, turma 19 - Tribo A 
+
 const { species } = data;
 
-const locationAnimals = () => {
-  return species
-    .reduce((acc, specie) => {
-      const { location } = specie;
+const locationAnimals = () => species
+  .reduce((acc, specie) => {
+    const { location } = specie;
 
-      acc[location] = species
-      .filter((animal) => animal.location === location)
-      .map((element) => element.name);
-  
-      return acc;
-    }, {});
-};
+    acc[location] = species
+    .filter((animal) => animal.location === location)
+    .map((element) => element.name);
 
-const namedAnimals = () => {
-  return species
-    .reduce((acc, specie) => {
-      const { location } = specie;
+    return acc;
+}, {});
 
-      acc[location] = species
-      .filter((animal) => animal.location === location)
-      .map((element) => {
-        const nameResidents = [];
-        element.residents.forEach((resident) => nameResidents.push(resident.name)) ; 
-        
-        const test = {};
-        test[element.name] = nameResidents;
-        return test;
-      });
-      return acc;
-    }, {});
-};
+const namedAnimals = () => species
+  .reduce((acc, specie) => {
+    const { location } = specie;
+
+    acc[location] = species
+    .filter((animal) => animal.location === location)
+    .map((element) => {
+      const nameResidents = [];
+      element.residents.forEach((resident) => nameResidents.push(resident.name)); 
+      
+      const test = {};
+      test[element.name] = nameResidents;
+      return test;
+    });
+    return acc;
+}, {});
+
+const sortNames = (options) => species
+  .reduce((acc, specie) => {
+    const { location } = specie;
+
+    acc[location] = species
+    .filter((animal) => animal.location === location)
+    .map((element) => {
+      const nameResidents = [];
+      element.residents.forEach((resident) => nameResidents.push(resident.name)); 
+      
+      const test = {};
+      test[element.name] = nameResidents.sort();
+      return test;
+    });
+    return acc;
+}, {});
+
+const sortNamesWithSex = (options) => species
+  .reduce((acc, specie) => {
+    const { location } = specie;
+
+    acc[location] = species
+    .filter((animal) => animal.location === location)
+    .map((element) => {
+      const nameResidents = [];
+      element.residents.forEach((resident) => {
+        if (resident.sex === options.sex) { 
+          nameResidents.push(resident.name);
+        }
+      }); 
+      
+      const test = {};
+      test[element.name] = nameResidents.sort();
+      return test;
+    });
+    return acc;
+}, {});
+
+const animalsBySex = (options) => species
+.reduce((acc, specie) => {
+  const { location } = specie;
+
+  acc[location] = species
+  .filter((animal) => animal.location === location)
+  .map((element) => {
+    const namesReisdents = [];
+    element.residents.forEach((resident) => {
+      if (resident.sex === options.sex) { 
+        namesReisdents.push(resident.name);
+      }
+    });
+    
+    const test = {};
+    test[element.name] = namesReisdents;
+    return test;
+  });
+  return acc;
+}, {});
+
+const validations = [
+  undefined,
+  { sex: 'female' },
+  { sex: 'female', sorted: true },
+  { includeNames: true },
+  { includeNames: true, sorted: true },
+  { includeNames: true, sex: 'female' },
+  { includeNames: true, sex: 'female', sorted: true },
+];
+
+const whatToDo = [
+  locationAnimals,
+  locationAnimals,
+  locationAnimals,
+  namedAnimals,
+  sortNames,
+  animalsBySex, 
+  sortNamesWithSex,
+];
 
 const getAnimalMap = (options) => {
-  if (!options || ) {
-    return locationAnimals();
-  }
+ let callback; 
 
-  if (!options.includeNames || options.includeNames === false) {
-    return locationAnimals();
-  }
+ validations.find((element, index) => {
+   if (JSON.stringify(element) === JSON.stringify(options)) {
+     callback = whatToDo[index];
+   }
+   return callback;
+ });
 
-  if (options.includeNames === true) {
-    return namedAnimals();
-  }
-
-  if (options.sorted === true) {
-    return 'oi';
-  }
+ return callback(options);
 };
 
 module.exports = getAnimalMap;
 
-console.log(getAnimalMap({ includeNames: true }));
-// console.log();
+console.log(getAnimalMap({ includeNames: true, sorted: true }));
