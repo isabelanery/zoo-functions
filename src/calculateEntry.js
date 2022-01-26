bf3eb0bff565be0a5f4d095441b966a6eb6286d4
isabelanery <isabelanery242@gmail.com>
2022-01-25 21:37:05 -0300

    Adiciona requisito 6.

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,36 @@
 const data = require('../data/zoo_data');
 
-function countEntrants(entrants) {
-  // seu código aqui!
-}
+const { prices } = data;
 
-function calculateEntry(entrants) {
-  // seu código aqui
-}
+const countEntrants = (entrants) => {
+  const counting = {
+    adult: 0,
+    child: 0,
+    senior: 0,
+  }; 
+
+  counting.adult = entrants.filter((adult) => adult.age >= 18 && adult.age < 50).length;
+
+  counting.child = entrants.filter((child) => child.age < 18).length;
+
+  counting.senior = entrants.filter((senior) => senior.age >= 50).length;
+
+  return counting;
+};
+
+const calculateEntry = (entrants) => {
+  if (!entrants) {
+    return 0; 
+  }
+
+  if (((typeof entrants) === 'object') && (Object.entries(entrants).length === 0)) {
+    return 0;
+  }
+
+  const [adult, child, senior] = Object.values(countEntrants(entrants));
+
+  return (adult * prices.adult + child * prices.child + senior * prices.senior);
+};
+ // return calculate(entrants);
 
 module.exports = { calculateEntry, countEntrants };
