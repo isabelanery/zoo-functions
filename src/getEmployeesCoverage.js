74ca239241bc427b1e718c9b99f2b99f61dae409
isabelanery <isabelanery242@gmail.com>
2022-01-27 12:25:07 -0300

    Adiciona 4/5 do requisito 10.

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,30 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
-}
+const { species, employees } = data;
+
+const createCoverageAll = () => employees.map((worker) => { 
+    const object = {
+      id: worker.id, 
+      fullName: `${worker.firstName} ${worker.lastName}`, 
+      species: worker.responsibleFor.map((id) => species.find((animal) => animal.id === id).name), 
+      locations: worker.responsibleFor.map((id) => species
+      .find((animal) => animal.id === id).location),
+    }; 
+    return object;
+});
+
+const getEmployeesCoverage = (obj) => {
+  if (!obj) {
+    return createCoverageAll();
+  }
+
+  if (obj.name !== undefined) {
+    return createCoverageAll().find((element) => element.fullName.includes(obj.name) === true);
+  }
+    
+  if (obj.id !== undefined) {
+    return createCoverageAll().find((element) => element.id === obj.id);
+  }
+};
 
 module.exports = getEmployeesCoverage;
